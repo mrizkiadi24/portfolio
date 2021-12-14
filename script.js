49493a523a8064533649d8c7074fbfdf3f30349f
Fikri Miftah Maulana <fikrimiftahm@gmail.com>
2021-12-14 21:29:34 +0700

    Cang

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-window.addEventListener('scroll', function () {
-  var header = document.querySelector('header');
-  header.classList.toggle('sticky', window.scrollY > 0);
-});
+// window.addEventListener('scroll', function () {
+//   var header = document.querySelector('header');
+//   header.classList.toggle('sticky', window.scrollY > 0);
+// });
 
-let navbar = document.querySelector('.navbar');
+// let navbar = document.querySelector('.navbar');
 
-document.querySelector('#menu-btn').onClick = () => {
-  navbar.classList.toggle('active');
-};
+// document.querySelector('#menu-btn').onClick = () => {
+//   navbar.classList.toggle('active');
+// };
+
+// window.onscroll = function () { scrollFunction() };
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("navbar").style.background = "#ffa51f";
+//   } else {
+//     document.getElementById("navbar").style.background = "#ffa51f";
+//   }
+// }
+
 var swiper = new Swiper('.review-slider', {
   spaceBetween: 20,
-  loop: true,
+  loop: false,
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
   },
   breakpoints: {
     640: {
-      slidesPerView: 1,
+      slidesPerView: 3,
     },
     768: {
-      slidesPerView: 2,
+      slidesPerView: 3,
     },
     1024: {
       slidesPerView: 3,
